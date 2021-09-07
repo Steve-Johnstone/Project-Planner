40e032cd382969081ad536ad3fe455e8ca7a6239
Steve Johnstone <t-sjohnstone@hotels.com>
2021-09-07 12:40:24 +0100

    Helper class and tooltip template added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,30 +9,61 @@ class DOMHelper {
     const element = document.getElementById(elementId);
     const destinationElement = document.querySelector(newDestinationSelector);
     destinationElement.append(element);
+    element.scrollIntoView({ behavior: 'smooth' });
   }
 }
 
-class Tooltip {
-  constructor(tooltipRemoved) {
+class Helper {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId) {
+      this.hostElement = document.getElementById(hostElementId);
+    } else {
+      this.hostElement = document.body;
+    }
+    this.insertBefore = insertBefore;
+  }
+  detach() {
+    this.element.remove();
+  }
+
+  attach(elementType, className, eventListenerFunction, data) {
+    const element = document.createElement(elementType);
+    element.className = className;
+    const tooltipTemplate = document.getElementById('tooltip');
+    const tooltipBody = document.importNode(tooltipTemplate.content, true);
+    tooltipBody.querySelector('p').textContent = data;
+    element.append(tooltipBody);
+
+    const hostElPosLeft = this.hostElement.offsetLeft;
+    const hostElPosTop = this.hostElement.offsetTop;
+    const hostElHeight = this.hostElement.clientHeight;
+    const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+
+    const x = hostElPosLeft + 20;
+    const y = hostElPosTop + hostElHeight - parentElementScrolling - 10;
+
+    element.style.position = 'absolute';
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
+
+    element.addEventListener('click', eventListenerFunction);
+    this.element = element;
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbeing' : 'beforeend',
+      this.element
+    );
+  }
+}
+class Tooltip extends Helper {
+  constructor(hostElementId, tooltipRemoved) {
+    super(hostElementId);
     this.tooltipRemoved = tooltipRemoved;
+    this.eventListenerFunction = this.closeTooltip;
   }
   closeTooltip = () => {
     this.detach();
     this.tooltipRemoved();
   };
-
-  detach() {
-    this.element.remove();
-  }
-
-  attach() {
-    const tooltipElement = document.createElement('div');
-    tooltipElement.className = 'card';
-    tooltipElement.innerText = 'This is your additional info mofo!';
-    tooltipElement.addEventListener('click', this.closeTooltip);
-    this.element = tooltipElement;
-    document.body.append(tooltipElement);
-  }
 }
 
 class ProjectItem {
@@ -59,15 +90,19 @@ class ProjectItem {
     if (this.hasTooltip) {
       return;
     }
-    const tooltip = new Tooltip(() => (this.hasTooltip = false));
-    tooltip.attach();
+    const projectElement = document.getElementById(this.id);
+    const data = projectElement.dataset.extraInfo;
+    const tooltip = new Tooltip(this.id, () => {
+      this.hasTooltip = false;
+    });
+    tooltip.attach('div', 'card', tooltip.eventListenerFunction, data);
     this.hasTooltip = true;
   }
 
   connectInfoButton() {
     const projectItemElement = document.getElementById(this.id);
     let infoBtn = projectItemElement.querySelector('button:first-of-type');
-    infoBtn.addEventListener('click', this.showTooltip);
+    infoBtn.addEventListener('click', this.showTooltip.bind(this));
   }
 
   update(updateProjectListFn, type) {
